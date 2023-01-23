87ca67340a13004de5c2262cf3fc9d06d997db59
albertjblack <albertjblack@outlook.com>
2023-01-23 09:55:59 -0700

    added print method

--- Lectures/wk2/PracticeArray.java
+++ Lectures/wk2/PracticeArray.java
@@ -9,4 +9,12 @@ class PracticeArray {
     int[] myArray2 = {2,5,7,8};
     System.out.println(myArray);
   }
+
+  static void printArray(int[] arr){
+    for (int i = 0; i<arr.length; i++){
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+  }
+
 };
