6e3b4f7a986bb7d7974142d13ea51d870adcc60e
albertjblack <albertjblack@outlook.com>
2023-01-23 09:25:42 -0700

    array defs

---
+++ Lectures/wk2/PracticeArray.java
@@ -0,0 +1,10 @@
+package Lectures.wk2;
+
+class PracticeArray {
+  public static void main(String[] args){
+    int[] myArray;
+    myArray  = new int[4];
+    int[] myArray2 = {2,5,7,8};
+    System.out.println(myArray);
+  }
+};
